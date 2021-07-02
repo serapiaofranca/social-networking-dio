97af10298180898c4d16dda499fa5b9169c18bc4
Rogerio A Serapiao <rserapa@gmail.com>
2021-07-02 16:28:11 -0300

    admin panel authenticate, sign_in root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :admins, skip: [:registrations]
+
+  namespace :admin do
+    root to: "home#index"
+  end
 end
