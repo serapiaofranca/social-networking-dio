a1f19d10435f4e047df4a48cba4fefb3662e01d2
Rogerio A Serapiao <rserapa@gmail.com>
2021-07-02 12:12:41 -0300

    create rspec test model_amin valid and not_valid

---
+++ spec/models/admin_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Admin, type: :model do
+  
+  it "admin is valid" do
+    admin = Admin.new(email: "admin@test.com", password: "123456")
+
+    expect(admin.email).to eql "admin@test.com"
+    expect(admin).to be_valid
+  end
+
+  it "admin is not value" do
+    admin = Admin.new(email: "admin@test.com", password: "")
+
+    expect(admin).to_not be_valid
+  end
+
+end
