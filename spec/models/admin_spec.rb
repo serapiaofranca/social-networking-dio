98ee2235cf05dc6a05054508e177d5b6fcaef4a0
Rogerio A Serapiao <rserapa@gmail.com>
2021-07-03 10:06:35 -0300

    migrate admin_name, resource admin_admins_list

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe Admin, type: :model do
   
   it "admin is valid" do
-    admin = Admin.new(email: "admin@test.com", password: "123456")
+    admin = Admin.new(name: "Admin First", email: "admin@test.com", password: "123456")
 
     expect(admin.email).to eql "admin@test.com"
     expect(admin).to be_valid
   end
 
   it "admin is not value" do
-    admin = Admin.new(email: "admin@test.com", password: "")
+    admin = Admin.new(email: "admin@test.com", password: "123456")
 
     expect(admin).to_not be_valid
   end
