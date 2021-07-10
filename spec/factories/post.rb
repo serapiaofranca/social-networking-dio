d34089f7e5eb395c25d7f8a7e5e4fb0966ba2a3a
Rogerio A Serapiao <rserapa@gmail.com>
2021-07-10 14:46:38 -0300

    test_model_post implemented

---
+++ spec/factories/post.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :post do
+        body {"The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. 
+               Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced
+               in their exact original form, accompanied by English versions from the 1914 translation by 
+               H. Rackham.factory"}        
+    end
+end
