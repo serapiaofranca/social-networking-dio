c09c18d7b8e3137725fd07e682eb80c9a4a7db50
Rogerio A Serapiao <rserapa@gmail.com>
2021-07-10 10:07:59 -0300

    user_sign_up implemented, session logged.

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,10 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+    before_action :configure_sign_up_params
+
+    protected 
+
+    def configure_sign_up_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :avatar])
+    end
+    
+end
