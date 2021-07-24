7066eec213b8b30a37146c0a0babe6f2e2a2dfed
Rogerio A Serapiao <rserapa@gmail.com>
2021-07-24 17:44:32 -0300

    developed like_unlike user_post

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "./like_toggle"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
