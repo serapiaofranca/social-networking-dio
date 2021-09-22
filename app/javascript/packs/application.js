f720dedb7a22f9373c2ba15c2ee39e7157e9687c
Rogerio A Serapiao <rserapa@gmail.com>
2021-09-21 21:43:28 -0300

    add toggle_comments funcionality

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,9 +13,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
+//require("jquery")
 
 import "./like_toggle"
+import "./comment_toggle"
 
 import $ from 'jquery';
 global.$ = jQuery;
